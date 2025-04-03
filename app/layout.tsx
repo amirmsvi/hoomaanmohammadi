aac1886aab02ba8411f4c8b6a195d626b1381e77
AmiR <amirmsvi66@gmail.com>
2025-04-03 00:36:54 -0400

    fixed horizontal scroll on mobile view

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,35 @@
-import type React from "react"
-import "@/app/globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from "react";
+import "@/app/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Hooman Mohammadi | Mechanical Engineer, EIT",
   description:
     "Professional portfolio of Hooman Mohammadi, Mechanical Engineer and EIT with Professional Engineers of Ontario (PEO).",
-    generator: 'v0.dev'
-}
+  viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+    <html lang="en" suppressHydrationWarning className="overflow-x-hidden">
+      <body className={`${inter.className} overflow-x-hidden`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
-
-
-
-import './globals.css'
